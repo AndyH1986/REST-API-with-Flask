3f09132b960653209db2bbe1c9565c9193f58602
Andreas Hohmann <a.hohmann2@gmx.de>
2023-06-25 17:25:45 +0200

    data validation with marshmallow

--- resources/item.py
+++ resources/item.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items, stores
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("items", __name__, description="Operations on items")
 
@@ -22,15 +23,8 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                404,
-                message="Bad request, ensure 'price' and 'name' are included in the JSON payload",
-            )
-
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data
@@ -45,18 +39,8 @@ class Store(MethodView):
     def get(self):
         return {"items": list(items.values())}
 
-    def post(self):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.",
-            )
-
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
