02e0008b7bbb78c4e0788266ac86f100292df708
Andreas Hohmann <a.hohmann2@gmx.de>
2023-07-09 18:43:58 +0200

    jwt

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -30,8 +33,12 @@ def create_app(db_url=None):
         db.create_all()
 
     api = Api(app)
+    app.config["JWT_SECRET_KEY"] = "jose"
+    jwt = JWTManager(app)
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
